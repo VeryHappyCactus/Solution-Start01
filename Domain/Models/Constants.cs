fb9d0cbc6dd831c89c0a4676ae81030b78a5d843
Maxim Paramonov <ncmail@ukr.net>
2020-08-21 15:47:15 +0300

    1

--- Domain/Models/Constants.cs
+++ Domain/Models/Constants.cs
@@ -4,11 +4,9 @@
     {
 #if DEBUG
         public const string DefaultConnection = "Server=DESKTOP-JPQ21PS;Database=Start01Bd;Trusted_Connection=True;MultipleActiveResultSets=true";
-        //public const string DefaultConnection = "Server=24.234.59.102;Database=Start01Bd;Persist Security Info=True;User ID=eau199;Password=SafeBroker%100";
 #else
-        //.102
         public const string DefaultConnection =
-            "Server=SP-VL;Database=Start01Bd;Persist Security Info=True;User ID=eau199;Password=SafeBroker%100";
+            "Server=SP-VL;Database=Start01Bd;Persist Security Info=True;User ID=xxxx;Password=xxxxxx";
 #endif
     }
 }
