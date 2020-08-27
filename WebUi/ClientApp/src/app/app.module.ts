107c3afe65b0dd7c987e677d3e1550120b621d7b
VeryHappyCactus <igor.kolisnychenko@gmail.com>
2020-08-27 18:58:51 +0300

    commit fix

--- WebUi/ClientApp/src/app/app.module.ts
+++ WebUi/ClientApp/src/app/app.module.ts
@@ -5,12 +5,12 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+//import { NavMenuComponent } from './nav-menu/nav-menu.component';
+//import { HomeComponent } from './home/home.component';
+//import { CounterComponent } from './counter/counter.component';
+//import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
-import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
+//import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
 import { LoginNewComponent } from 'src/app/login-new/login-new.component';
 //import { TransferMainComponent } from './transfer/transfer-main/transfer-main.component';
